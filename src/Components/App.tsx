2621eb2d80fde51874de0368d7f2cd1b859da8d6
Owen Masback <omasback@gmail.com>
2019-04-16 10:54:01 -0700

    add homepage component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Styleguide from './Styleguide'
 import '../styles/global'
 import '../styles/theme'
+import Homepage from './Homepage'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header" />
           <Route path="/styleguide" component={Styleguide} />
+          <Route path="/" component={Homepage} exact />
         </div>
       </Router>
     )
---
+++ src/Components/Homepage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import withStyles, { WithStyles } from 'react-jss'
+import theme from '../styles/theme'
+
+interface Props extends WithStyles<typeof styles> {}
+
+const Homepage: React.FunctionComponent<Props> = ({ classes }) => (
+  <div className={classes.root}>Homepage</div>
+)
+
+const styles = {
+  root: {},
+}
+
+export default withStyles(styles)(Homepage)
