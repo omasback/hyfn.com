57517da05b054d563b7467eb96f3accff52becac
Owen Masback <omasback@gmail.com>
2019-04-17 15:03:19 -0700

    convert HomepageHero to class component

--- src/Components/pages/home/HomepageHero.tsx
+++ src/Components/pages/home/HomepageHero.tsx
@@ -1,27 +1,33 @@
 import * as React from 'react'
-import { createStylesHook } from '../../../helpers/createStylesHook'
 import theme from '../../../styles/theme'
+import { withStyles, createStyles, WithStyles } from '@material-ui/styles'
 
-interface Props {}
-
-// Material-UI's JSS implementation. Uses Hook instead of HOC
-// can also use tagged template literals or HOC's if you want
+// Material-UI's JSS implementation.
+// Must use HOC version for class component
 // https://material-ui.com/css-in-js/basics/
 // autocomplete soon without createStyles? https://github.com/mui-org/material-ui/pull/15366
-const useStyles = createStylesHook({
+const styles = createStyles({
   h1: {
     margin: 0,
     color: theme.colors.blue,
   },
 })
 
-const HomepageHero: React.FunctionComponent<Props> = () => {
-  const classes = useStyles()
-  return (
-    <div>
-      <h1 className={classes.h1}>HYFN</h1>
-    </div>
-  )
+interface Props extends WithStyles<typeof styles> {}
+
+class HomepageHero extends React.Component<Props> {
+  componentDidMount() {
+    console.log('hello!')
+  }
+
+  render() {
+    const { classes } = this.props
+    return (
+      <div>
+        <h1 className={classes.h1}>HYFN</h1>
+      </div>
+    )
+  }
 }
 
-export default HomepageHero
+export default withStyles(styles)(HomepageHero)
