bcd81e438bce845695909aa5f63fdadf858f6a63
Owen Masback <omasback@gmail.com>
2019-05-24 14:57:34 -0700

    better theme change behavior

--- src/components/display/ThemeSetter.tsx
+++ src/components/display/ThemeSetter.tsx
@@ -8,6 +8,8 @@ import constants from 'styles/constants'
 // This Component uses absolute positioning to fill the area of a parent or ancestor element,
 // So make sure to apply a positioning to the ancestor that you wish to track.
 
+const rootMargins = ['49%', '0%', '49%', '0%']
+
 const useStyles = makeStyles(
   {
     root: {
@@ -17,7 +19,17 @@ const useStyles = makeStyles(
       right: 0,
       bottom: 0,
       pointerEvents: 'none',
+      // border: '1px solid #ffff00', // to help with debugging
     },
+    // for debugging, this visuallizes the intersection root area:
+    // rootMargin: {
+    //   position: 'fixed',
+    //   top: rootMargins[0],
+    //   right: rootMargins[1],
+    //   bottom: rootMargins[2],
+    //   left: rootMargins[3],
+    //   backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    // },
   },
   { name: 'ThemeSetter' }
 )
@@ -25,17 +37,21 @@ const useStyles = makeStyles(
 const ThemeSetter: React.FunctionComponent<{
   color?: string // desired text color and other foreground elements
   backgroundColor?: string // desired background color of the page
+  parent?: string // parent component name to help with debugging
 }> = ({
   color = constants.colors.darkGray,
   backgroundColor = constants.colors.lightGray,
+  parent,
 }) => {
   const { setTheme } = useTheme<Theme>()
   const classes = useStyles()
   const [ref, inView] = useInView({
-    threshold: 0.5,
+    threshold: 0,
+    rootMargin: rootMargins.map(n => '-' + n).join(' '),
   })
 
   React.useEffect(() => {
+    // console.log(parent, inView)
     if (inView) {
       setTheme(color, backgroundColor)
     }
@@ -47,12 +63,15 @@ const ThemeSetter: React.FunctionComponent<{
       // if you put the cleanup function in an effect with dependencies,
       // it will run every time dependencies change,
       // prematurely clearing the theme (bad)
-      console.log('without dependency change')
       setTheme()
     }
   }, [setTheme])
 
-  return <div ref={ref} className={classes.root} />
+  return (
+    <div ref={ref} className={classes.root}>
+      {/* <div className={classes.rootMargin} /> */}
+    </div>
+  )
 }
 
 export default ThemeSetter
