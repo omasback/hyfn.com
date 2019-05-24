c46525bbb8f4e2b2e2e45a68137254f74ed51482
Owen Masback <omasback@gmail.com>
2019-05-24 16:14:42 -0700

    Create ThemeTransition.tsx

---
+++ src/components/display/ThemeTransition.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import * as cx from 'classnames'
+import { Theme } from 'components/App'
+
+const useStyles = makeStyles<Theme>(
+  theme => ({
+    root: {
+      transition: 'background-color 0.5s, color 0.5s',
+    },
+    color: {
+      color: theme.color,
+    },
+    backgroundColor: {
+      backgroundColor: theme.backgroundColor,
+    },
+  }),
+  { name: 'ThemeTransition' }
+)
+
+const ThemeTransition: React.FunctionComponent<{
+  color?: boolean
+  backgroundColor?: boolean
+  className?: string
+}> = ({ color, backgroundColor, className, children }) => {
+  const classes = useStyles()
+  return (
+    <div
+      className={cx(
+        classes.root,
+        {
+          [classes.color]: color,
+          [classes.backgroundColor]: backgroundColor,
+        },
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default ThemeTransition
